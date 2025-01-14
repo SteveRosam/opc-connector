5da892679d11556297068f96a9fabc3ecdfb377b
Steve Rosam <steve+newdemo@quix.io>
2025-01-14 13:44:21 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -28,7 +28,7 @@ class SubHandler:
         # print("New data change event", node, val)
         print("New data change event")
         print("--------------")
-        print(node.ns)
+        print(node.namespace)
         print(node.nodeid.Identifier)
 
         print("--------------")

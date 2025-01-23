8f78c4800cd464713028efcdde49145939ad2d78
Steve Rosam <steve+newdemo@quix.io>
2025-01-23 14:36:51 +0000

    Edited 'main.py'

--- Downsampling/main.py
+++ Downsampling/main.py
@@ -5,19 +5,36 @@ from quixstreams import Application
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
+app = Application(consumer_group="raw-to-table-v5", auto_offset_reset="earliest", use_changelog_topics=True)
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+sdf = sdf.apply(lambda row: row["payload"], expand=True)
 
-sdf.print()
-sdf.to_topic(output_topic)
+def expand_values_to_columns(row: dict):
+    new_row = {}
+    for key in row["values"]:
+        new_row[row["name"] + "-" + key] = row["values"][key]
+
+    new_row["timestamp"] = row["time"]
+    
+    return new_row
+
+sdf = sdf.apply(expand_values_to_columns)
+
+sdf = sdf.hopping_window(5000, 250).reduce(lambda state, row: { **state, **row}, lambda row: row).final()
+
+sdf = sdf.apply(lambda row:{
+    "timestamp": row["start"],
+    **row["value"]
+})
+
+sdf = sdf.update(lambda row: print(row))
+
+sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(sdf)

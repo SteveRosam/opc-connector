e0cf36d63e8fe99e45fc6f7a83fe12439dc377d4
Steve Rosam <steve+newdemo@quix.io>
2025-01-24 10:09:08 +0000

    Edited 'main.py'

--- Downsampling/main.py
+++ Downsampling/main.py
@@ -26,6 +26,8 @@ def expand_values_to_columns(value: float):
 sdf = sdf.apply(expand_values_to_columns)
 sdf.print()
 
+sdf.apply(lambda row: print(row['value']))
+
 # sdf = sdf.hopping_window(5000, 250).reduce(lambda state, row: { **state, **row}, lambda row: row).final()
 
 # sdf = sdf.apply(lambda row:{

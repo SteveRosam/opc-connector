fb67396a13d8ebfaab4779c5741e3b6f236aaa81
Steve Rosam <steve+newdemo@quix.io>
2025-01-24 11:08:27 +0000

    Edited 'main.py'

--- Streamlit Dashboard/main.py
+++ Streamlit Dashboard/main.py
@@ -23,7 +23,7 @@ def start():
 st.title("WebSocket Live Data Viewer")
 
 # Authentication and options
-if "is_connected" not in st.session_state or st.session_state == False:
+if "is_connected" not in st.session_state or st.session_state.is_connected == False:
     st.text_input("Websocket root URL", type="default", key="wss_root_url", value="ws://localhost")
     st.text_input("Websocket port", type="default", key="wss_port", value="8080")
     st.text_input("Websocket Username", type="default", key="wss_username", value="")

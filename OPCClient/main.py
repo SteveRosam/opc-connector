47457952a7d9cd9a307e1ff4a0f57dd77cce07da
Steve Rosam <steve+newdemo@quix.io>
2025-02-12 17:41:06 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -95,6 +95,8 @@ async def main():
             for child in children:
                 child_browse_name = await child.read_browse_name()
                 print(child_browse_name)
+                myvar = await client.nodes.root.get_child(f"/Objects/2:{browse_name}/2:{child_browse_name}")
+                print(myvar)
                 # print(f"  Child Node: {child_browse_name}, Value: {child_value}")
             
 

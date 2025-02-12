60945b98d0ddc9a74268cf4c72a4bc1a706f96cd
Steve Rosam <steve+newdemo@quix.io>
2025-02-12 17:23:59 +0000

    Edited 'main.py'

--- OPCServer/main.py
+++ OPCServer/main.py
@@ -75,8 +75,8 @@ async def main():
     
     # create directly some objects and variables
     my_object = await server.nodes.objects.add_object(idx, "3D_PRINTER_1")
-    my_variable = await my_object.add_variable(idx, "THERMO_PROBE_1", 6.7)
-    my_variable_2 = await my_object.add_variable(idx, "THERMO_PROBE_2", 6.7)
+    my_variable = await my_object.add_variable(idx, "THERMO_PROBE_1", 99.8)
+    my_variable_2 = await my_object.add_variable(idx, "THERMO_PROBE_2", 97.6)
     await my_variable.set_writable()  # Set MyVariable to be writable by clients
     
     

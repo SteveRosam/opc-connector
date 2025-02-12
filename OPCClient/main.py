954c7da5f4e3bc4a171a28fb110d438cd7a13bde
Steve Rosam <steve+newdemo@quix.io>
2025-02-12 17:47:49 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -96,7 +96,7 @@ async def main():
                 child_browse_name = await child.read_browse_name()
                 print("++++++++++")
                 print(child_browse_name)
-                param_string = f"/Objects/2:{browse_name}/2:{child_browse_name}"
+                param_string = f"/Objects/2:Server/2:ServerArray"
                 print("---------")
                 print(param_string)
                 myvar = await client.nodes.root.get_child(param_string)

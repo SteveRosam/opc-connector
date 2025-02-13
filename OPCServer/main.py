31ea130bae55ccbc2021ab77f83fa0c938cd0136
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 12:46:29 +0000

    Edited 'main.py'

--- OPCServer/main.py
+++ OPCServer/main.py
@@ -132,7 +132,7 @@ async def main():
             await server.write_attribute_value(probe2.nodeid, ua.DataValue(scaled_value_2))
             await asyncio.sleep(0.1)
             
-                        # Update probe1 with a scaled sine value
+            # Update probe1 with a scaled sine value
             scaled_value_1 = scale_sin_to_range(MIN_VALUE, MAX_VALUE)
             await server.write_attribute_value(probe1.nodeid, ua.DataValue(scaled_value_1))
             await asyncio.sleep(0.1)

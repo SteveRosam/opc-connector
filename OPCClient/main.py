097f8fce01a76e15c1de3caa1b39cd356da8627f
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 14:10:01 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -52,7 +52,7 @@ class SubHandler:
 
         browse_name = await node.read_browse_name()
         friendly_name = browse_name.Name
-        id = f'{node.nodeid.NamespaceIndex}__{friendly_name}'
+        id = f'{OPC_NAMESPACE}__{friendly_name}'
 
         print(friendly_name)
 

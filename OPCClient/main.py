b4620f4246d0ce13bba39eff478a41520a7f798a
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 11:30:52 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -116,7 +116,7 @@ async def main():
 
                 # #         if param_string not in tracked_values:
                     print(type(child_id))
-                    if child_id in ['12','13']:
+                    if child_id in [12,13]:
                         myvar = await client.nodes.root.get_child(param_string)
                         tracked_values[param_string] = myvar
                         print(tracked_values)

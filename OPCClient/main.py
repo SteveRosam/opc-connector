68c3c1aca4b488fe73c4ea531d93ceb74b723d9e
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 11:36:19 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -143,7 +143,7 @@ async def main():
                     child_id = child.nodeid.Identifier
                     print(child_id)
                         
-                    param_string = f"/Objects/{obj_id}:{browse_name.Name}/{child_id}:{child_browse_name.Name}"
+                    param_string = f"/Objects/2:{browse_name.Name}/2:{child_browse_name.Name}"
                 #     print("---------")
                     print(param_string)
 

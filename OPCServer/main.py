1fddbb71a3c9c344b5c3872f04b1e90084e5e783
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 12:42:43 +0000

    Edited 'main.py'

--- OPCServer/main.py
+++ OPCServer/main.py
@@ -123,17 +123,19 @@ async def main():
         # Send an initial value / this could be the current value or a default.
         await server.write_attribute_value(probe1.nodeid, ua.DataValue(99.8))
         await server.write_attribute_value(probe2.nodeid, ua.DataValue(0.9))
+        await asyncio.sleep(0.1)
 
         while True:
-            await asyncio.sleep(0.1)
             
             # Update probe1 with a scaled sine value
-            # scaled_value_1 = scale_sin_to_range(MIN_VALUE, MAX_VALUE)
-            # await server.write_attribute_value(probe1.nodeid, ua.DataValue(scaled_value_1))
+            scaled_value_1 = scale_sin_to_range(MIN_VALUE, MAX_VALUE)
+            await server.write_attribute_value(probe1.nodeid, ua.DataValue(scaled_value_1))
+            await asyncio.sleep(0.1)
             
             # Update probe2 with a different scaled sine value or another logic
             scaled_value_2 = scale_sin_to_range(MIN_VALUE*2, MAX_VALUE*2)  # or another logic
             await server.write_attribute_value(probe2.nodeid, ua.DataValue(scaled_value_2))
+            await asyncio.sleep(0.1)
 
 
 if __name__ == "__main__":

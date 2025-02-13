ae447c94c7a75d8353e87c82008d5671322e5b06
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 11:20:27 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -114,6 +114,8 @@ async def main():
                 #             tracked_values[param_string] = myvar
                     print("~_~_~_~_~_~_~_~_")
                     print(myvar)
+                    print(myvar.nodeid)
+                    print(myvar.nodeid.Identifier)
                     print("~_~_~_~_~_~_~_~_")
                 #         # print(f"  Child Node: {child_browse_name}, Value: {child_value}")
                 except Exception as e:

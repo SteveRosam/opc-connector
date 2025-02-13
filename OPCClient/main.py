0df7838637e15f5a6a54b9c8d32e7ca9dd8457dc
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 13:58:09 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -77,148 +77,151 @@ async def main():
     opc_namespace = os.environ["OPC_NAMESPACE"]
     tracked_values = {}
 
-    async with Client(url=opc_url) as client:
+    try:
+        async with Client(url=opc_url) as client:
 
-        # # Access the Objects node
-        # objects_node = client.nodes.objects
-        
-        # # Get all child nodes of the Objects node
-        # objects = await objects_node.get_children()
-        
-        # # Iterate over each object node
-        # for obj in objects:
-        #     # Get the object's browse name and NodeId
-        #     browse_name = await obj.read_browse_name()
-        #     node_id = obj.nodeid
+            # # Access the Objects node
+            # objects_node = client.nodes.objects
             
-        #     # Print the object's browse name and NodeId
-        #     print(f"Object: {browse_name.Name}, NodeId: {node_id}")
+            # # Get all child nodes of the Objects node
+            # objects = await objects_node.get_children()
             
-        #     # Optionally, print the children of each object
-        #     children = await obj.get_children()
-        #     for child in children:
-        #         child_browse_name = await child.read_browse_name()
-        #         child_node_id = child.nodeid
-        #         print(f"  Child Node: {child_browse_name.Name}, NodeId: {child_node_id}")
+            # # Iterate over each object node
+            # for obj in objects:
+            #     # Get the object's browse name and NodeId
+            #     browse_name = await obj.read_browse_name()
+            #     node_id = obj.nodeid
+                
+            #     # Print the object's browse name and NodeId
+            #     print(f"Object: {browse_name.Name}, NodeId: {node_id}")
+                
+            #     # Optionally, print the children of each object
+            #     children = await obj.get_children()
+            #     for child in children:
+            #         child_browse_name = await child.read_browse_name()
+            #         child_node_id = child.nodeid
+            #         print(f"  Child Node: {child_browse_name.Name}, NodeId: {child_node_id}")
 
-        # print("END END END END END END END END END END END END END ")
-
-
-
-
-
-        namespace_array_node = client.get_node("i=2255")  # NodeId for NamespaceArray
-        namespace_array = await namespace_array_node.read_value()
-        target_namespace_index = 0
-        # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-        # Print all namespaces
-        for index, namespace in enumerate(namespace_array):
-            # print(f"Namespace Index: {index}, Namespace URI: {namespace}")
-            pass
-        
-        if opc_namespace in namespace_array:
-            target_namespace_index = namespace_array.index(opc_namespace)
-            # print(f"Target Namespace URI '{opc_namespace}' is at index {target_namespace_index}")
-        else:
-            # print(f"Target Namespace URI '{opc_namespace}' not found")
-            pass
+            # print("END END END END END END END END END END END END END ")
 
 
 
-        # _logger.info("Root node is: %r", client.nodes.root)
-        # _logger.info("Objects node is: %r", client.nodes.objects)
 
-        # Node objects have methods to read and write node attributes as well as browse or populate address space
-        # _logger.info("Children of root are: %r", await client.nodes.root.get_children())
 
-        # Get the Objects node
-        objects_node = client.nodes.objects
-        # Get all child nodes of the Objects node
-        objects = await objects_node.get_children()
-        
-        # Iterate over each object node
-        for obj in objects:
-            # Get the object's browse name
-            browse_name = await obj.read_browse_name()
-            # print(f"ObjectBrowseName: {browse_name}")
-            obj_id = obj.nodeid.Identifier
-            # print(obj_id)
-
-            # if browse_name.Name in ["Device0001", "3D_PRINTER_1"]:
-                # print("fooo")
-                # Optionally, get and print the children of each object
-            children = await obj.get_children()
-            # print("===========")
-            # print(children)
-            for child in children:
-                child_browse_name = await child.read_browse_name()
-                # print("++++++++++")
-                # print(f"CHILDBrowseName: {child_browse_name}")
-
-                try:
-                    # print(child.nodeid)
-                    child_id = child.nodeid.Identifier
-                    # print(child_id)
-                        
-                    param_string = f"/Objects/{target_namespace_index}:{browse_name.Name}/{target_namespace_index}:{child_browse_name.Name}"
-                #     print("---------")
-                    # print(param_string)
-
-                # #         if param_string not in tracked_values:
-                    # print(type(child_id))
-                    # print(child_id in [12,13])
-                    if child_id in [12,13]:
-                        print("~_~_~_~_~_~_~_~_")
-                        myvar = await client.nodes.root.get_child(param_string)
-                        tracked_values[param_string] = myvar
-                        print(tracked_values)
-                        # print(myvar)
-                        # print("~_~_~_~_~_~_~_~_")
-                #         # print(f"  Child Node: {child_browse_name}, Value: {child_value}")
-                except Exception as e:
-                    print(e)
+            namespace_array_node = client.get_node("i=2255")  # NodeId for NamespaceArray
+            namespace_array = await namespace_array_node.read_value()
+            target_namespace_index = 0
+            # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+            # Print all namespaces
+            for index, namespace in enumerate(namespace_array):
+                # print(f"Namespace Index: {index}, Namespace URI: {namespace}")
+                pass
             
+            if opc_namespace in namespace_array:
+                target_namespace_index = namespace_array.index(opc_namespace)
+                # print(f"Target Namespace URI '{opc_namespace}' is at index {target_namespace_index}")
+            else:
+                # print(f"Target Namespace URI '{opc_namespace}' not found")
+                pass
 
-        # # Now getting a variable node using its browse path
-        # myvar = await client.nodes.root.get_child("/Objects/2:MyObject/2:MyVariable")
-        # obj = await client.nodes.root.get_child("Objects/2:MyObject")
-        # _logger.info("myvar is: %r", myvar)
 
-        # subscribing to a variable node
-        subscriptions = {}
-        handles = {}
-        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        print(tracked_values)
-        for val in tracked_values:
 
-            # Get the node for the current value
-            myvar = await client.nodes.root.get_child(val)  # Adjust this line to get the correct node
+            # _logger.info("Root node is: %r", client.nodes.root)
+            # _logger.info("Objects node is: %r", client.nodes.objects)
 
-            # Create a handler and subscription for each node
-            handler = SubHandler()
-            sub = await client.create_subscription(10, handler)
+            # Node objects have methods to read and write node attributes as well as browse or populate address space
+            # _logger.info("Children of root are: %r", await client.nodes.root.get_children())
 
-            # Subscribe to data changes for the node
-            handle = await sub.subscribe_data_change(myvar)
-
-            # Store the subscription and handle
-            subscriptions[val] = sub
-            handles[val] = handle
+            # Get the Objects node
+            objects_node = client.nodes.objects
+            # Get all child nodes of the Objects node
+            objects = await objects_node.get_children()
             
-            # Optional: Sleep to stagger subscriptions
-            await asyncio.sleep(0.1)
+            # Iterate over each object node
+            for obj in objects:
+                # Get the object's browse name
+                browse_name = await obj.read_browse_name()
+                # print(f"ObjectBrowseName: {browse_name}")
+                obj_id = obj.nodeid.Identifier
+                # print(obj_id)
+
+                # if browse_name.Name in ["Device0001", "3D_PRINTER_1"]:
+                    # print("fooo")
+                    # Optionally, get and print the children of each object
+                children = await obj.get_children()
+                # print("===========")
+                # print(children)
+                for child in children:
+                    child_browse_name = await child.read_browse_name()
+                    # print("++++++++++")
+                    # print(f"CHILDBrowseName: {child_browse_name}")
+
+                    try:
+                        # print(child.nodeid)
+                        child_id = child.nodeid.Identifier
+                        # print(child_id)
+                            
+                        param_string = f"/Objects/{target_namespace_index}:{browse_name.Name}/{target_namespace_index}:{child_browse_name.Name}"
+                    #     print("---------")
+                        # print(param_string)
+
+                    # #         if param_string not in tracked_values:
+                        # print(type(child_id))
+                        # print(child_id in [12,13])
+                        if child_id in [12,13]:
+                            print("~_~_~_~_~_~_~_~_")
+                            myvar = await client.nodes.root.get_child(param_string)
+                            tracked_values[param_string] = myvar
+                            print(tracked_values)
+                            # print(myvar)
+                            # print("~_~_~_~_~_~_~_~_")
+                    #         # print(f"  Child Node: {child_browse_name}, Value: {child_value}")
+                    except Exception as e:
+                        print(e)
+                
+
+            # # Now getting a variable node using its browse path
+            # myvar = await client.nodes.root.get_child("/Objects/2:MyObject/2:MyVariable")
+            # obj = await client.nodes.root.get_child("Objects/2:MyObject")
+            # _logger.info("myvar is: %r", myvar)
+
+            # subscribing to a variable node
+            subscriptions = {}
+            handles = {}
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print(tracked_values)
+            for val in tracked_values:
+
+                # Get the node for the current value
+                myvar = await client.nodes.root.get_child(val)  # Adjust this line to get the correct node
+
+                # Create a handler and subscription for each node
+                handler = SubHandler()
+                sub = await client.create_subscription(10, handler)
+
+                # Subscribe to data changes for the node
+                handle = await sub.subscribe_data_change(myvar)
+
+                # Store the subscription and handle
+                subscriptions[val] = sub
+                handles[val] = handle
+                
+                # Optional: Sleep to stagger subscriptions
+                await asyncio.sleep(0.1)
 
 
-        
-        # # calling a method on server
-        # res = await obj.call_method("2:multiply", 3, "klk")
-        # _logger.info("method result is: %r", res)
-        while run:
-            await asyncio.sleep(1)
+            
+            # # calling a method on server
+            # res = await obj.call_method("2:multiply", 3, "klk")
+            # _logger.info("method result is: %r", res)
+            while run:
+                await asyncio.sleep(1)
 
-        # unsubscribe the handler
-        # await sub.unsubscribe(handle)
-        # await sub.delete()
+            # unsubscribe the handler
+            # await sub.unsubscribe(handle)
+            # await sub.delete()
+        except ConnectionError ce:
+            print(ce)
 
 if __name__ == "__main__":
     try:

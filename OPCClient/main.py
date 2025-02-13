8fd39d3e9c731bb9b56496019b584c9614007a84
Steve Rosam <steve+newdemo@quix.io>
2025-02-13 16:05:57 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -115,11 +115,11 @@ async def main():
                     print("CHILD BROWSE NAME")
                     print(child_browse_name)
                     try:
-                        # child_id = child.nodeid.Identifier
+                        child_id = child.nodeid.Identifier
                         param_string = f"/Objects/{target_namespace_index}:{browse_name.Name}/{target_namespace_index}:{child_browse_name.Name}"
-                        # if child_id in [12,13]:
-                        myvar = await client.nodes.root.get_child(param_string)
-                        tracked_values[param_string] = myvar
+                        if child_id in [12,13]:
+                            myvar = await client.nodes.root.get_child(param_string)
+                            tracked_values[param_string] = myvar
                     except Exception as e:
                         print(e)
 

fda9c4c7cec84cbb4fc8e21c63ec32ee5dddb61d
Steve Rosam <steve+newdemo@quix.io>
2025-02-14 10:10:03 +0000

    Edited 'main.py'

--- OPCClient/main.py
+++ OPCClient/main.py
@@ -63,6 +63,7 @@ class SubHandler:
         print(f"Data change event for node {machine_name}: {val}")
         id = f'{OPC_NAMESPACE}/{machine_name}'
 
+        print(data)
         # Extract the DataValue from the data parameter
         data_value = data.monitored_item.Value
         # Extract the source timestamp
